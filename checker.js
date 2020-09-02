8c6d5ddfedd63d89f63e6d180e817a3e20cdb91d
roopam-kumar <46059596+roopam-kumar@users.noreply.github.com>
2020-09-02 11:37:25 +0530

    update functions

--- checker.js
+++ checker.js
@@ -1,40 +1,47 @@
 const expect = require('chai').expect;
-var x=0, y=0, z=0;
+//var x = 0;
+//var x=0, y=0, z=0;
 
 function vitalsAreOk(bpm, spo2, respRate) {
 	
-	 return (bpm < 70 || bpm > 150 || spo2 < 90 || respRate < 30 || respRate > 95)  ? false : true
+	 bpmHigh(bpm);
+	 bpmLow(bpm);
+	 spo2Low(spo2);
+	 respRateHigh(respRate);
+	 respRateLow(respRate);
+	
     
 }
-/*function findAbnormality(bpm, spo2, respRate)
+function bpmHigh(bpm)
 {
-		if(bpm < 70)
-			x=1;
-		else if(bpm > 150)
-			x=2;
-		if(spo2 < 90)
-			y=3;
-		if( respRate < 30)
-			z=4;
-		else if( respRate > 95)
-			z=5;
-		if(x==1)
-			console.log('Abnormality : BPM  Value:' + bpm + ' Breach: Low');
-		else if(x==2)
-			console.log('Abnormality : BPM Value:' + bpm + ' Breach: High');
-		if(y==3)
-			console.log('Abnormality : SPO2 Value:' + spo2 + ' Breach: Low');
-		if(z==4)
-			console.log('Abnormality : respRate Value:' + respRate + ' Breach: Low');
-		else if(z==5)
-			console.log('Abnormality : respRate Value:' + respRate + ' Breach: High');
-		
-	
-}*/
+	if(bpm>150)
+		console.log('Abnormality : BPM Value:' + bpm + ' Breach: High');
+}
+function bpmLow(bpm)
+{
+	if(bpm<70)
+		console.log('Abnormality : BPM  Value:' + bpm + ' Breach: Low');
+}
+function spo2Low(spo2)
+{
+	if(spo2 < 90)
+		console.log('Abnormality : SPO2 Value:' + spo2 + ' Breach: Low');
+}
+function respRateHigh(respRate)
+{
+	if(respRate > 95)
+		console.log('Abnormality : respRate Value:' + respRate + ' Breach: High');
+}
+function respRateLow(respRate)
+{
+	if(respRate < 30)
+		console.log('Abnormality : respRate Value:' + respRate + ' Breach: Low');
+}
 		
 
-expect(vitalsAreOk(100, 95, 70)).to.be.true;
-expect(vitalsAreOk(50, 95, 70)).to.be.false;
-//findAbnormality(50,95,70);
-//findAbnormality(156,34,22);
+//expect(vitalsAreOk(100, 95, 70)).to.be.true;
+//expect(vitalsAreOk(50, 95, 70)).to.be.false;
+vitalsAreOk(100,95,70);
+vitalsAreOk(50,95,70);
+vitalsAreOk(190,88,100);
 console.log('checker is done');
